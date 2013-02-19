3fbefa26a114d0a80545cf9516a5ec646cca35c9
davetayls <dave@the-taylors.org>
2013-02-19 18:57:06 +0000

    added jshintrc template

---
+++ jshintrc/template.js
@@ -0,0 +1,28 @@
+/*
+ * grunt-init jshintrc
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a jshintrc';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '.jshintrc';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({}, [], function(err, props) {
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // All done!
+    done();
+  });
+
+};
